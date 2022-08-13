0d38a5f145d45c4416cd662bf142b514bc43bb9e
Tayyib Gbondo <gbondotahib72@gmail.com>
2022-08-13 10:49:13 -0700

    added some text in the js file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('hello world')
+
+let userName = prompt('Hi Please enter your name')
+
+function greeting(){
+  if (userName){
+    alert(`hello ${userName} welcome to our store`)
+  } else{
+    alert('please make sure to enter some text')
+  }
+}
+greeting()
